02433d0834068b8402456699ddb3082cd7a99b8d
ZEAB-H <abelhailemichael30@gmail.com>
2022-10-14 21:38:49 +0300

    added the API entry point for the app

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -1,6 +1,7 @@
 import Lane from '../../components/Lane/Lane';
 import './Board.css';
 import React from 'react'
+import { useState,useEffect } from 'react';
 
 const lanes = [
   { id: 1, title: 'To Do' },
@@ -10,10 +11,41 @@ const lanes = [
 ];
 
 function Board() {
+  const [loading,setLoading]= useState(false);
+  const [tasks,setTasks]=useState([]);
+  const [error,setError]=useState('');
+  
+  useEffect(()=>{
+    async function fectchData(){
+      try{
+        const tasks=await fetch(`https://my-json-server.typicode.com/ZEAB-H/TaskDb/tasks`,
+        );
+        const result= await tasks.json();
+        
+        if (result){
+          setTasks(result);
+          setLoading(false);
+        }
+      }
+       catch(e){
+        setLoading(false);
+        setError(e.message);
+       }
+      }
+
+    
+    fectchData();
+  },[]);
+
   return (
     <div className='Board-wrapper'>
       {lanes.map((lane) => (
-        <Lane key={lane.id} title={lane.title} />
+        <Lane key={lane.id} 
+        title={lane.title} 
+        loading={loading}
+        error={error}
+        tasks={tasks.filter((task)=>
+            task.lane ===lane.id)}/>
       ))}
     </div>
   );
