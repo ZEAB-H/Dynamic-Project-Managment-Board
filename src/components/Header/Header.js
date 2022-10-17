7a602860bf88c33cc8149fb4f07934bd7673d28d
ZEAB-H <abelhailemichael30@gmail.com>
2022-10-17 09:47:44 +0300

    changed pmpmp to pmb

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,7 +22,7 @@ background-color: rgb(68, 124, 236);
 function Header() {
   return (
     <HeaderWrapper  >
-      <h1>PMP:project management board</h1>
+      <h1>PMB:project management board</h1>
     </HeaderWrapper>
   )
 }
