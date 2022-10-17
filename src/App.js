8c55a96dde8f9ac8b2d64986f1560abfa6fbf464
ZEAB-H <abelhailemichael30@gmail.com>
2022-10-17 08:09:58 +0300

    added global style to app js, remove index.css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import styled from 'styled-components';
 import Board from './pages/Board/Board';
 import Header from './components/Header/Header';
 import Backlog from './pages/Backlog/Backlog';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle=createGlobalStyle`
+margin: 0;
+font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+  sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;`;
+
+
 
 const APP =styled.div`
 text-align: center;
@@ -10,11 +21,15 @@ background: beige;`;
 
 function App() {
   return (
+    <>
+    <GlobalStyle>
     <APP className='App'>
       <Header />
       <Board />
       <Backlog/>
     </APP>
+    </GlobalStyle>
+    </>
   );
 }
 
