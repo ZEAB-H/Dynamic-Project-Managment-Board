10e5c6cd04a4f3e2102fd1b049379b0c7e8be317
ZEAB-H <abelhailemichael30@gmail.com>
2022-10-17 07:49:35 +0300

    added styled component to task component

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,15 +1,30 @@
                                    
 import React from 'react';
+import styled from 'styled-components';
 
+const TaskWrapper= styled.div`
+    background:lightblue;
+    padding:20px;
+    border-radius:20px;
+    margin:0% 5% 5% 5%;
+
+    h3{
+        width: 100%;
+        margin:0;
+    }
+    p{
+        color:darkgreen;
+    }
+`;
 
 function Task({id,onDragStart,title,body}){
     return(
-        <div className='Task-wrapper'
+        <TaskWrapper 
         draggable
         onDragStart={(e)=> onDragStart(e,id)}>
             <h3>{title}</h3>
             <p>{body}</p>
-        </div>
+        </TaskWrapper>
     );
 }
 
