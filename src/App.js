614cf3ff24dbebb2a36b1d96262dfbf5dfbe0c94
ZEAB-H <abelhailemichael30@gmail.com>
2022-10-17 08:03:16 +0300

    added styled component to appjs, removed custom css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
 import Board from './pages/Board/Board';
 import Header from './components/Header/Header';
 import Backlog from './pages/Backlog/Backlog';
 
+const APP =styled.div`
+text-align: center;
+background: beige;`;
+
 function App() {
   return (
-    <div className='App'>
+    <APP className='App'>
       <Header />
       <Board />
       <Backlog/>
-    </div>
+    </APP>
   );
 }
 
